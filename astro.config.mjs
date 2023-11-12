e3dfcf74feb1b08abfd110743672cf2d40b6d036
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2023-11-12 11:38:39 +0600

    :tada: refactored

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+import keystatic from '@keystatic/astro';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), markdoc(), tailwind()],
+  integrations: [mdx(), sitemap(), react(), markdoc(), tailwind(), keystatic()],
   output:'hybrid'
 });
