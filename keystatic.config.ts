e62eb2c71cd470c6b282bb8365381fb7ddd11c8c
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2023-11-13 00:30:03 +0600

    blogging working

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -11,14 +11,26 @@ export default config({
       slugField: "title",
       path: "src/content/posts/*",
       format: { contentField: "content" },
+
       schema: {
         title: fields.slug({ name: { label: "Title" } }),
+        date: fields.date({
+          label: "Event date",
+          description: "The date of the event",
+        }),
         content: fields.document({
           label: "Content",
           formatting: true,
           dividers: true,
           links: true,
           images: true,
+        }),
+        avatar: fields.image({
+          label: "Avatar",
+          description: "The avatar for this user",
+          // This will output the images in the "public" directory
+          directory: "public/images/avatars",
+          publicPath: "/images/avatars/",
         }),
       },
     }),
